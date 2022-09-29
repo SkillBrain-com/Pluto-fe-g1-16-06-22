9aeaa9cd2cd8268373a784d9ef2789e77deb20c0
deiu25 <d3iu25@gmail.com>
2022-09-29 17:51:38 +0300

    hw-taskViewe

--- Badulescu Andrei/Task-Manager/src/components/task-viewer/TaskViewer.js
+++ Badulescu Andrei/Task-Manager/src/components/task-viewer/TaskViewer.js
@@ -1,31 +1,31 @@
-// import TaskCard from "./components/task-card/TaskCard";
-// import App from "./App";
+import TaskCard from "./components/task-card/TaskCard";
+import App from "./App";
 
-// function TaskViewer(props) {
-//   return (
-//     (<App data={props.data} />),
-//     (
-//       <div className="app-container">
-//         <TaskCard
-//           id={data[0].id}
-//           status={data[0].status}
-//           name={data[0].name}
-//           dueDate={data[0].dueDate}
-//         />
-//         <TaskCard
-//           id={data[1].id}
-//           status={data[1].status}
-//           name={data[1].name}
-//           dueDate={data[1].dueDate}
-//         />
-//         <TaskCard
-//           id={data[2].id}
-//           status={data[2].status}
-//           name={data[2].name}
-//           dueDate={data[2].dueDate}
-//         />
-//       </div>
-//     )
-//   );
-// }
-// export default TaskViewer;
+function TaskViewer(props) {
+  return (
+    (<App data={props.data} />),
+    (
+      <div className="app-container">
+        <TaskCard
+          id={data[0].id}
+          status={data[0].status}
+          name={data[0].name}
+          dueDate={data[0].dueDate}
+        />
+        <TaskCard
+          id={data[1].id}
+          status={data[1].status}
+          name={data[1].name}
+          dueDate={data[1].dueDate}
+        />
+        <TaskCard
+          id={data[2].id}
+          status={data[2].status}
+          name={data[2].name}
+          dueDate={data[2].dueDate}
+        />
+      </div>
+    )
+  );
+}
+export default TaskViewer;
